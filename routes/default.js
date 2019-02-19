82aadec5b29b2496a96ba41aebdfe3a5f54a3484
ssskram <paul.marks@pittsburghpa.gov>
2019-02-19 12:15:30 -0500

    bot response

--- routes/default.js
+++ routes/default.js
@@ -10,6 +10,17 @@ router.post('/',
         let payload = req.body
         console.log(payload)
         res.sendStatus(200)
+        fetch('https://slack.com/api/chat.postMessage', {
+                method: 'POST',
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + process.env.BOT_TOKEN,
+                    'Content-Type': 'application/json'
+                }),
+                body: JSON.stringify({
+                    "text": "Hello <@U29GKE80K>! How are we today?",
+                    "channel": "GG9K9JYEM"
+                })
+            })
     }
 )
 
