67ded213e408eb616faeca86037f1ce351d9affc
ssskram <paul.marks@pittsburghpa.gov>
2019-02-19 12:39:54 -0500

    makin it modular

--- routes/default.js
+++ routes/default.js
@@ -9,24 +9,37 @@ router.post('/',
     async function (req, res) {
         let payload = req.body
         console.log(payload)
-        console.log(req.body.event.channel)
         res.sendStatus(200)
-        fetch('https://slack.com/api/chat.postMessage', {
-            method: 'POST',
-            headers: new Headers({
-                'Authorization': 'Bearer ' + process.env.BOT_TOKEN,
-                'Content-Type': 'application/json'
-            }),
-            body: JSON.stringify({
-                "text": "Hello, <@" + req.body.event.user + ">! What can I do you for?",
-                "channel": req.body.event.channel
-            })
-        })
+        if (req.body.event.channel == "GG9K9JYEM") {
+            greeting(req.body.event.user)
+        } else {
+            fourOhThree(req.body.event.channel)
+        }
     }
 )
 
 const greeting = user => {
-
+    postMessage({
+        "text": "Hello, <@" + user + ">! What can I do you for?",
+        "channel": "GG9K9JYEM"
+    })
 }
 
+const fourOhThree = channel => {
+    postMessage({
+        "text": "Sorry friend, I can't talk here.",
+        "channel": channel
+    })
+}
+
+const postMessage = message => {
+    fetch('https://slack.com/api/chat.postMessage', {
+        method: 'POST',
+        headers: new Headers({
+            'Authorization': 'Bearer ' + process.env.BOT_TOKEN,
+            'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify(message)
+    })
+}
 module.exports = router
