018aeda36888c05fbe8e10b14be2a077c8aa8366
ssskram <ssskram@ssskram.local>
2019-03-18 11:20:48 -0400

    fixed routes

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use(require('morgan')('combined'))
 
 // routes
 app.use("/", require('./routes/default'))
-app.use("activeDirectory", require('./routes/activeDirectory.js'))
-app.use("azMonitor", require('./routes/azMonitor.js'))
+app.use("/activeDirectory", require('./routes/activeDirectory.js'))
+app.use("/azMonitor", require('./routes/azMonitor.js'))
 
 // production error handler
 if (app.get('env') === 'production') {
